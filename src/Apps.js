7145c39a602abc8191c68301138c2ff6d263eec9
kiplangatmike <m.kiplangat@alustudent.com>
2022-12-11 19:43:49 +0200

    css update

--- src/Apps.js
+++ src/Apps.js
@@ -2,21 +2,36 @@ const { useState } = require("react");
 const { default: fetchWeather } = require("./api/fetchWeather");
 
 const Apps = () => {
-    const [search, setSearch] = useState('');
-    const [weather, setWeather] = useState([]);
+  const [search, setSearch] = useState("");
+  const [weather, setWeather] = useState([]);
 
-    const handleSearch = async (e) => {
-        if(e.key === 'Enter'){
-            const data = await fetchWeather(search);
-            setWeather(data);
-            setSearch('');
-        }
+  const handleSearch = async (e) => {
+    if (e.key === "Enter") {
+      const data = await fetchWeather(search);
+      setWeather(data);
+      setSearch("");
     }
-    return (
+  };
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          placeholder="enter your timezone"
+          onKeyPress={handleSearch}
+          onChange={(e) => setQuery(e.target.value)}
+          value={search}
+        />
         <div>
-            <div>
-                <input type="text" placeholder="enter your timezone" onKeyPress={handleSearch} onChange={(e)=> setQuery(e.target.value)} value={search}/>
-            </div>
+          <h2>
+            <span>{weather.city}</span>
+            <sup>{weather.main.city}</sup>
+          </h2>
+          <div>
+            {weather.main.temp}<sup>&dec;C</sup>
+          </div>
         </div>
-    )
-} 
+      </div>
+    </div>
+  );
+};
